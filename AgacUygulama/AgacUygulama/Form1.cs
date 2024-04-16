9f7496268b22b97d6112fe7a28ec963a1bb902eb
yesimaygul <yesimaygul7@gmail.com>
2024-04-16 06:40:51 +0300

    Lab7-Trees

---
+++ AgacUygulama/AgacUygulama/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgacUygulama
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IkiliAgacDugumu kok = new IkiliAgacDugumu(4);
+            kok.sag = new IkiliAgacDugumu(12);
+            kok.sag.sag = new IkiliAgacDugumu(1);
+            kok.sag.sol = new IkiliAgacDugumu(7);
+
+            kok.sol = new IkiliAgacDugumu(6);
+            kok.sol.sol = new IkiliAgacDugumu(45);
+
+            kok.PreOrder(kok);
+            MessageBox.Show(kok.dugumler);
+            kok.dugumler = "";
+
+            kok.InOrder(kok);
+            MessageBox.Show(kok.dugumler);
+            kok.dugumler = "";
+
+            kok.PostOrder(kok);
+            MessageBox.Show(kok.dugumler);
+            kok.dugumler = "";
+
+
+            MessageBox.Show(kok.DugumSayisi(kok).ToString());
+            MessageBox.Show(kok.YaprakSayisi(kok).ToString());
+
+
+        }
+    }
+}
---
+++ AgacUygulama/AgacUygulama/IkiliAgacDugumu.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgacUygulama
+{
+    public class IkiliAgacDugumu
+    {
+        public object veri;
+        public IkiliAgacDugumu sol;
+        public IkiliAgacDugumu sag;
+        public string dugumler;
+        public IkiliAgacDugumu(object veri)
+        {
+            this.veri = veri;
+            sol = null;
+            sag = null;
+        }
+        public void PreOrder(IkiliAgacDugumu dugum)
+        {
+            if (dugum == null)
+                return;
+
+            Ziyaret(dugum);
+
+            PreOrder(dugum.sol);
+            PreOrder(dugum.sag);
+        }
+
+        private void Ziyaret(IkiliAgacDugumu dugum)
+        {
+            dugumler += dugum.veri + " ";
+        }
+        public void PostOrder(IkiliAgacDugumu dugum)
+        {
+            if (dugum == null)
+                return;
+
+            PostOrder(dugum.sol);
+            PostOrder(dugum.sag);
+            Ziyaret(dugum);
+        }
+
+        public void InOrder(IkiliAgacDugumu dugum)
+        {
+            if (dugum == null)
+                return;
+
+            InOrder(dugum.sol);
+            Ziyaret(dugum);
+            InOrder(dugum.sag);
+        }
+
+        public int DugumSayisi(IkiliAgacDugumu dugum)
+        {
+            int count = 0;
+            if (dugum != null)
+            {
+                count = 1;
+                count += DugumSayisi(dugum.sol);
+                count += DugumSayisi(dugum.sag);
+
+            }
+            return count;
+        }
+        public int YaprakSayisi(IkiliAgacDugumu dugum)
+        {
+            int count = 0;
+            if (dugum != null)
+            {
+                if ((dugum.sol==null)&&(dugum.sag==null))
+                {
+                    count = 1;
+                }
+                else
+                { 
+                    count = count + YaprakSayisi(dugum.sol) + YaprakSayisi(dugum.sag);
+                }
+            }
+            return count;
+        }
+    }
+}
